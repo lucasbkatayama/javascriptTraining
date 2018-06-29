5b5266af6e5348f3ef6eb7097338aa7fcc1b7cc1
lucaskatayama <lucaskatayama12@gmail.com>
2018-06-28 22:49:03 -0300

    Finished

--- starterapp/js/app.js
+++ starterapp/js/app.js
@@ -1,3 +1,29 @@
-$('.search-button').click(function() {
-  alert("Button Clicked!");
-})
+var displayWikipediaData = () => {
+
+  var $linksElement = $('#links');
+  var searchString = $('#searchString').val();
+  var wikipediaUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchString + "&format=json&callback=wikiCallback";
+
+  $.ajax({
+    url: wikipediaUrl,
+    dataType: "jsonp",
+    jsonp: "callback",
+    success: function(res) {
+      var linkList = res[1];
+      linkList.forEach(function(item) {
+        var url = 'https://en.wikipedia.org/wiki/'
+          + item;
+        $linksElement.append('<li><a href="' + url + '">' + item + '</a></li>');
+        return url;
+
+
+      })
+
+
+    }
+
+  });
+  return false;
+};
+
+$('#form').submit(displayWikipediaData);
